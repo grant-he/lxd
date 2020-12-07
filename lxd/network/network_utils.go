65ad2e67ab4a8fffce5eeda1ab50b11cb60d2ad7
JLRDRAGON92000 <jlrdragon@gmail.com>
2020-12-07 16:08:57 -0600

    lxd/network: GetNeighbourIPs passes through to iproute
    Signed-off-by: Jared Rankin <jared.rankin@utexas.edu>

--- lxd/network/network_utils.go
+++ lxd/network/network_utils.go
@@ -764,30 +764,7 @@ func GetHostDevice(parent string, vlan string) string {
 
 // GetNeighbourIPs returns the IP addresses in the neighbour cache for a particular interface and MAC.
 func GetNeighbourIPs(interfaceName string, hwaddr string) ([]net.IP, error) {
-	addresses := []net.IP{}
-
-	// Look for neighbour entries for IPv.
-	out, err := shared.RunCommand("ip", "neigh", "show", "dev", interfaceName)
-	if err == nil {
-		for _, line := range strings.Split(out, "\n") {
-			// Split fields and early validation.
-			fields := strings.Fields(line)
-			if len(fields) != 4 {
-				continue
-			}
-
-			if fields[2] != hwaddr {
-				continue
-			}
-
-			ip := net.ParseIP(fields[0])
-			if ip != nil {
-				addresses = append(addresses, ip)
-			}
-		}
-	}
-
-	return addresses, nil
+	return iproute.GetNeighbourIPs(interfaceName, hwaddr)
 }
 
 // GetLeaseAddresses returns the lease addresses for a network and hwaddr.
